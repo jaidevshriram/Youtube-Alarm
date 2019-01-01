39b762974c5e960b2221d6141028f6f9bbc19dd3
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-01-01 12:30:04 +0300

    First test run of Alarm

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
-function time() {
+//Current Time Variables
+var current_hour;
+var current_minute;
+
+//Alarm Time Variables
+var alarm_hour;
+var alarm_minute;
+var alarm = 0;
+
+function currenttime() {
 	var time = new Date();
-	var hours = time.getHours();
-	var minutes = time.getMinutes();
+	var hours = current_hour = time.getHours();
+	var minutes = current_minute = time.getMinutes();
 	var seconds = time.getSeconds();
 	hours = update(hours);
 	minutes = update(minutes);
 	second = update(seconds);
 	$("#current-time").html(hours + " : " + minutes);
+
+	if(alarm == 0) {
+		$('#player').css("display","none");
+	}
 }
 
 function update(time)
@@ -15,8 +28,6 @@ function update(time)
 		time="0"+time;
 	return time;
 }
-
-setInterval(time, 1000);
 
 var player;
 function Activate( jQuery ) {
@@ -30,6 +41,8 @@ function Activate( jQuery ) {
     'onReady': onPlayerReady
   }
 });
+
+  player.pauseVideo();
 }
 
 function onPlayerReady(event) {
@@ -37,11 +50,27 @@ function onPlayerReady(event) {
 }
 
 function setalarm() {
-	var hour = $("#hour").val();
-	var minutes = $("#minutes").val();
+	var hour = alarm_hour = $("#hour").val();
+	var minutes = alarm_minute = $("#minutes").val();
 	hour = update(hour);
 	minutes = update(minutes);
 	$('#Set-Alarm-Modal').find('.time').text(hour + " : " + minutes);
 	$('#Set-Alarm-Modal').modal();
 	$('#set-alarm').css("display","none");
+
+	alarm = 1;
 }
+
+function check() {
+	if(alarm == 1)
+	{
+		if(current_hour == alarm_hour && current_minute == alarm_minute)
+		{
+			$('#player').css("display","block");
+			player.playVideo();
+		}
+	}
+}
+
+setInterval(currenttime, 1000);
+setInterval(check, 1000);
